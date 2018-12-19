1beec3537756114d7cfeb3b62a6fd27e346b18b5
Angelo Rüggeberg <s3xy4ngyc@googlemail.com>
2018-12-19 18:44:30 +0100

    Updated polzbar theme

--- prezto-scripts/android-sdk.zsh
+++ prezto-scripts/android-sdk.zsh
@@ -1,4 +1,9 @@
 export ANDROID_HOME="$HOME/Library/Android/sdk"
+
+if [ "$(uname)" = "Linux" ]; then
+  export ANDROID_HOME="$HOME/Android/Sdk"
+fi
+
 export PATH="$ANDROID_HOME/platform-tools:$PATH"
 export PATH="$ANDROID_HOME/tools/bin:$PATH"
 export PATH="$ANDROID_HOME/tools:$PATH"
